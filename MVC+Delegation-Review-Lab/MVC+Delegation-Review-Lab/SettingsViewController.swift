662179055b72b8c4fd9cb5aba1d307666324ec10
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-20 15:17:56 -0500

    - Added UI elements - started coding for segues and tableView

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Gregory Keeley on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    var movies: Movie?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.
+    }
+}
